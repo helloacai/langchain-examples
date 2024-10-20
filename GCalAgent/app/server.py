aaeb51bdc757cbc3474c57ce52ae956cb48008ae
Jigish Patel <tskraja@gmail.com>
2024-10-19 20:16:18 -0700

    better server

--- GCalAgent/app/server.py
+++ GCalAgent/app/server.py
@@ -27,8 +27,24 @@ async def post_thread(request: Request):
     inputs = {"messages": [system_message(), HumanMessage(content=request.requestRef)]}
     messages = []
     async for chunk in graph.astream(inputs, config=config, stream_mode="values"):
-        messages.append(chunk)
-        logger.info(chunk)
+        chunk_message = chunk["messages"][-1]
+        logger.info(chunk_message)
+        if chunk_message["type"] == "ai" or chunk_message["type"] == "tool":
+            logger.info("sending back ai or tool message")
+            message = {}
+            if isinstance(chunk_message["content"], str):
+                message["content"] = chunk_message["content"]
+            else:
+                for content in chunk_message["content"]:
+                    if content["type"] == "text":
+                        message["content"] = content["text"]
+            if chunk_message["response_metadata"]["stop_reason"] == "end_turn":
+                message["status"] = "complete"
+            else:
+                message["status"] = "info"
+            messages.append(message)
+    if messages[-1]["status"] == "info":
+        messages[-1]["status"] = "waiting"
     return messages
 
 @app.patch("/thread")
@@ -41,6 +57,22 @@ async def patch_thread(request: Request):
             )
     messages = []
     async for chunk in graph.astream(None, config=config, stream_mode="values"):
-        messages.append(chunk)
-        logger.info(chunk)
+        chunk_message = chunk["messages"][-1]
+        logger.info(chunk_message)
+        if chunk_message["type"] == "ai" or chunk_message["type"] == "tool":
+            logger.info("sending back ai or tool message")
+            message = {}
+            if isinstance(chunk_message["content"], str):
+                message["content"] = chunk_message["content"]
+            else:
+                for content in chunk_message["content"]:
+                    if content["type"] == "text":
+                        message["content"] = content["text"]
+            if chunk_message["response_metadata"]["stop_reason"] == "end_turn":
+                message["status"] = "complete"
+            else:
+                message["status"] = "info"
+            messages.append(message)
+    if messages[-1]["status"] == "info":
+        messages[-1]["status"] = "waiting"
     return messages
