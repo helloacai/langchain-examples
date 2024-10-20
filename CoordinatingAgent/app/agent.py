e9e75e8b38dff8cea8ad80d3bf89a648c7bab2d8
Ketul Jay Patel <ketul.jay.patel@gmail.com>
2024-10-19 20:36:36 -0700

    added latest coord updates

--- CoordinatingAgent/app/agent.py
+++ CoordinatingAgent/app/agent.py
@@ -2,11 +2,14 @@ from langchain_core.messages import AIMessage
 from langchain_core.tools import tool
 from langgraph.prebuilt import ToolNode
 from typing import Literal
+from typing_extensions import Annotated
+
 
 from langchain_anthropic import ChatAnthropic
 from langgraph.graph import StateGraph, MessagesState
 from langgraph.prebuilt import ToolNode
 from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.prebuilt import InjectedState
 import requests
 
 import os
@@ -51,9 +54,9 @@ wallet = getWallet()
 print(wallet.default_address)
 
 @tool
-def call_agent(agent: int, request: str):
+def call_agent(agent: int, request: str, state: Annotated[dict, InjectedState]):
     """Call the specific agent with a request""" # Right now just spoofing this. Once Agent wallet is created this will be a contract call via CDP
-    print(agent, request)
+    print(agent, request, state)
     if agent == 1:
         return "You did it!"
     elif agent == 2:
@@ -109,11 +112,11 @@ workflow.add_edge("tools", "agent")
 
 graph = workflow.compile()
 
-# for chunk in graph.stream(
-#     {"messages": [
-#         ("system", "You are a coordinating agent. When evaluating a user's request you will first get a list of all the agents you can call on and their capabilities. This list will show the agent name, short description, and their unique identifier. Based on their short description you will decide on 1-3 helper agents to call on using your call_agent tool. When calling this tool you will provide the Agent's corresponding identifier and the request that you would like the agent to complete."),
-#         ("human", "What is the tallest building in the united states?")
-#     ]},
-#     stream_mode="values",
-# ):
-#     chunk["messages"][-1].pretty_print()
+for chunk in graph.stream(
+    {"messages": [
+        ("system", "You are a coordinating agent. When evaluating a user's request you will first get a list of all the agents you can call on and their capabilities. This list will show the agent name, short description, and their unique identifier. Based on their short description you will decide on 1-3 helper agents to call on using your call_agent tool. When calling this tool you will provide the Agent's corresponding identifier and the request that you would like the agent to complete."),
+        ("human", "What is the tallest building in the united states?")
+    ]},
+    stream_mode="values",
+):
+    chunk["messages"][-1].pretty_print()
